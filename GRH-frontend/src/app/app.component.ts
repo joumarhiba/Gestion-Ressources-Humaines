db80c4667f0440b2e6097502af1bab8ea7e612b8
joumarhiba <joumhiba7@gmail.com>
2023-01-09 00:48:10 +0100

    initial commit

---
+++ GRH-frontend/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+declare function greetings(): any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'GRH-frontend';
+
+  ngOnInit(): void {
+    greetings();
+}
+}
