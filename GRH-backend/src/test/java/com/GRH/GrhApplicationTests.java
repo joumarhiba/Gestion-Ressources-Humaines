b6b896c5fc5104f8dc9e229f0249fb06c36eebc5
joumarhiba <joumhiba7@gmail.com>
2023-02-11 21:02:33 +0100

    initialiser la partie backend

---
+++ GRH-backend/src/test/java/com/GRH/GrhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GRH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
