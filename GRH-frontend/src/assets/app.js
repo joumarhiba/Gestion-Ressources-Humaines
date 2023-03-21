859bcf84c7dfa00cd0456fca0510b51034e69af9
joumarhiba <hjoumar@norsys.fr>
2023-03-21 10:22:38 +0000

    la partie formation frontend

--- GRH-frontend/src/assets/app.js
+++ GRH-frontend/src/assets/app.js
@@ -6,5 +6,32 @@ function greetings() {
     hamburger_menu.addEventListener("click", () => {
     container.classList.toggle("active");
     });
+
+    const slider = document.querySelector('.slider-container');
+const slides = document.querySelectorAll('.slider-item');
+const prevArrow = document.querySelector('.arrow-left');
+const nextArrow = document.querySelector('.arrow-right');
+const slideWidth = slides[0].offsetWidth;
+let position = 0;
+
+function moveSlider(direction) {
+  if (direction === 'left') {
+    position += slideWidth;
+  } else {
+    position -= slideWidth;
+  }
+  
+  if (position > 0) {
+    position = -(slides.length - 1) * slideWidth;
+  } else if (position < -(slides.length - 1) * slideWidth) {
+    position = 0;
+  }
+  
+  slider.style.transform = `translateX(${position}px)`;
+}
+
+prevArrow.addEventListener('click', () => moveSlider('left'));
+nextArrow.addEventListener('click', () => moveSlider('right'));
+
     
 }
